e6f337f25c745289c0dd66007262d887cfcc8cb8
monklock <alex.rtischew@yandex.com>
2025-12-27 21:49:40 +0300

    change layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,12 +1,9 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-    </q-header>
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <AppBottomNav />
   </q-layout>
 </template>
-
-<script setup>
-</script>
