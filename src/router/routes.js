80e55220466b96aaa0389c16e54117851154f705
monklock <alex.rtischew@yandex.com>
2025-12-27 21:34:41 +0300

    add page routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,24 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      {
+        path: '',
+        component: () => import('pages/HomePage.vue'),
+      },
+      {
+        path: 'buses',
+        component: () => import('pages/SettingPage.vue'),
+      },
+      {
+        path: 'buses',
+        component: () => import('pages/BusListPage.vue'),
+      },
+      {
+        path: 'buses',
+        component: () => import('pages/TaxiListPage.vue'),
+      },
+    ],
   },
 
   // Always leave this as last one,
