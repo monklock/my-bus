6ee829872db16a20ab0dddbb64c53da02d417d92
monklock <alex.rtischew@yandex.com>
2025-12-27 23:22:01 +0300

    Merge remote-tracking branch 'origin/main' into develop (#10)
    * change md
    
    * change layout
    
    * add logo
    
    * add icogenie
    
    * generate icons
    
    * add all pages
    
    * add page routes
    
    * change layout
    
    * add component list
    
    * add component list
    
    * remove indexPage
    
    * added entry point skeleton
    
    * fix home
    
    * delete IndexPage.vue
    
    * Revert "delete IndexPage.vue"
    
    This reverts commit 58e7cb854e50e3cb856b520ddc24621fa2b4c971.
    
    * change md
    
    * change md
    
    * Remove IndexPage.vue

--- src/pages/BusListPage.vue
+++ src/pages/BusListPage.vue
@@ -1,6 +1,10 @@
 <template>
+<<<<<<<< HEAD:src/pages/BusListPage.vue
   <q-page padding>
     <!-- content -->
+========
+  <q-page class="flex flex-center">
+>>>>>>>> origin/main:src/pages/IndexPage.vue
   </q-page>
 </template>
 
